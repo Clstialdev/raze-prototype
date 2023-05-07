82d2508a52ebd5e80591cb14e230898c532041fd
aymen <aymenboudchichap@gmail.com>
2023-05-07 17:42:25 +0200

    fix to GamingTitle

--- src/components/GamingTitle.tsx
+++ src/components/GamingTitle.tsx
@@ -1,10 +1,10 @@
 const GamingTitle = () => {
   return (
     <div className="absolute left-0 top-0">
-      <div className="ppBold ml-[8%] mt-[50vh] text-[70px] leading-[20px] text-white md:text-[150px] lg:ml-[12%]  lg:mt-[35vh] lg:text-[220px] lg:leading-[135px]">
+      <div className="ppBold ml-[10%] mt-[50vh] text-[70px] leading-[20px] text-white md:text-[150px] lg:ml-[12%]  lg:mt-[35vh] lg:text-[220px] lg:leading-[135px]">
         <div className="flex  ">
-          <h1 className="h-fit bg-zinc-800">GAMING</h1>
-          <p className="pp  relative mb-0 ml-[95px] mt-4 flex  w-40 whitespace-nowrap  text-base text-green-300 md:mb-7 md:mt-8 md:text-[30px]  lg:ml-[190px] lg:mt-20 lg:text-[38px]">
+          <h1 className="h-fit">GAMING</h1>
+          <p className="pp  relative mb-0 ml-[95px] mt-4 flex  w-40 whitespace-nowrap  text-base text-green-300 md:mb-7 md:ml-[130px] md:mt-8 md:text-[30px] lg:mb-[-20px] lg:ml-[190px] lg:mt-32 lg:text-[38px]">
             RAZE UP!
           </p>
         </div>
