43dbcc57db9d360a85164af77aab1332da4c22d2
retconned <retcnned@gmail.com>
2023-06-03 12:13:53 +0200

    adds fill animation for store

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -80,9 +80,10 @@ const Store = () => {
       <div className=" 2xl:mt-[50px] mt-[50%] h-screen w-full text-center md:mt-[35%] lg:mt-[5%]">
         <div className="headingPro text-[120px] leading-[110px] text-white  md:leading-[112px] lg:text-[340px] lg:leading-[280px]">
           <h1>OUR</h1>
-          <div className="relative">
+          <div className="group relative">
+            <h1 className="textShadow ">OFFICIAL</h1>
+            <div className="absolute left-[478px] top-[122px] z-20 mt-[6px] h-4 border-[1px] bg-white duration-[780ms] ease-in-out md:h-4 md:w-[400px]  lg:h-[34px] lg:w-[0px] lg:group-hover:w-[950px]"></div>
             <div className="absolute left-1/2 top-1/2 mt-[6px] h-4 w-[350px] -translate-x-[50%] -translate-y-[50%] border-[1px] border-[#C2C2C2] bg-black md:h-4  md:w-[400px] lg:h-[34px] lg:w-[950px]"></div>
-            <h1 className="textShadow">OFFICIAL</h1>
           </div>
           <h1>STORE</h1>
         </div>
