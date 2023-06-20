ed79d29d62a70d814d6e0c895dd86403a37e5388
Clstialdev <rusty75001@gmail.com>
2023-06-20 21:03:46 +0100

    tezxt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
             "The Language Is Our Common Bond And The Fun Never Stops. Whether You're A Grammar Guru, A Vocab Virtuoso, Or Just A Lover Of All Things English, We've Got Something For You. Our Sub-communities Cater To All Interests, From The Hardcore Gamers Who Dominate The Leaderboards, To The Bookworms Who Devour Literature Faster Than A Speeding Bullet. From Literary Luncheons To Movie Marathons, Gaming Tournaments To Women's Empowerment Workshops, We're Always Finding New Ways To Showcase Our Passions And Connect With Each Other. So Grab A Cuppa, Kick Back, And Join Us On This Wild And Wacky Ride. Together, We'll Create A Community That's Chock-full Of Good Times, Great Memories, And Of Course, Plenty Of Puns Along The Way!",
           images: [
             {
-              url: "https://razesociety.com/_next/image?url=%2Fimages%2Fnewsletterbg2.png&w=3840&q=75",
+              url: "/images/newsletterbg2.png",
               alt: "Preview Image",
             },
           ],
